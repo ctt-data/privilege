96bfaa12a5b0a8b302357c3eeefee418003afec2
testyourprivilege <40883460+testyourprivilege@users.noreply.github.com>
2018-07-06 13:31:23 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,3 +4,26 @@ $( document ).ready(function() {
 	
     $("#main_title").text("My Title!")
 });
+
+var score = 0;
+$("#q1_yes").click(function() {
+  score = score + 1;
+  $("#score").text("Score: " + score)
+  $("#score").addClass("invisible")
+});
+var score2 = 0;
+$("#q1_no").click(function() {
+  score2 = score2 + 1;
+  $("#score2").text("score: "+score2)
+});
+var score=0;
+$("#q2_yes").click(function(){
+  score=score+1;
+  $("#score").text("score:"+score)
+                   
+                   });
+var score= 0;
+$("#q2_no").click(function(){
+  score2=score2+1;
+$("#score2").text("score:"+score2)  
+});
