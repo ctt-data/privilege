15993898573c951328b200ef4b7e296a9c610a70
testyourprivilege <40883460+testyourprivilege@users.noreply.github.com>
2018-07-17 18:16:32 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,25 +5,39 @@ $( document ).ready(function() {
     $("#main_title").text("My Title!")
 });
 
-var score = 0;
-$("#q1_yes").click(function() {
-  score = score + 1;
-  $("#score").text("Score: " + score)
-  $("#score").addClass("invisible")
+var scoreYes = 0;
+$(".button-yes").click(function() {
+  scoreYes = scoreYes + 1;
 });
-var score2 = 0;
-$("#q1_no").click(function() {
-  score2 = score2 + 1;
-  $("#score2").text("score: "+score2)
+var scoreNo = 0;
+$(".button-no").click(function() {
+  scoreNo = scoreNo + 1;
 });
-var score=0;
-$("#q2_yes").click(function(){
-  score=score+1;
-  $("#score").text("score:"+score)
-                   
-                   });
-var score= 0;
-$("#q2_no").click(function(){
-  score2=score2+1;
-$("#score2").text("score:"+score2)  
+var count = 0;
+$(".button-yes, .button-no").click(function(){
+  count=count+1;
+  if(count>questions.length){
+    // show result
+    $("#scoreYes").text(scoreYes);
+    $("#scoreNo").text(scoreNo);
+    $(".button-yes, .button-no").hide();
+  }else {
+  $(".question").text(questions[count]);
+  }
 });
+
+var questions = ["Are you right handed?",
+                 "Is English your first language?",
+                 "Does one or both of your parents have a college degree?",
+                 "Can you find Band-Aids at mainstream stores designed to blend in with or match your skin tone?",
+                 "Do you rely, or have relied, primarily on public transportation?",
+                 "Have you attended previous schools with people you felt were like yourself?",
+                 "Do you constantly feel unsafe walking alone at night?",
+                 "Does your household employs help as servants, gardeners, etc.?",
+                 "Are you able to move through the world without fear of sexual assault?",
+                 "Have you studied the culture of your ancestors in elementary school?",
+                 "Have you ever been able to play a significant role in a project or activity because of a talent you          gained previously?",  
+                 
+                ];
+
+$(".question").text(questions[0]);
