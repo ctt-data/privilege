2398b3e32c68067b578f4aa69af1e75d0a71a5cf
testyourprivilege <40883460+testyourprivilege@users.noreply.github.com>
2018-07-18 20:17:27 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -14,30 +14,46 @@ $(".button-no").click(function() {
   scoreNo = scoreNo + 1;
 });
 var count = 0;
-$(".button-yes, .button-no").click(function(){
-  count=count+1;
-  if(count>questions.length){
+$(".button-yes, .button-no").click(function() {
+  count = count + 1;
+  // end of survey.Display of result
+  if (count > questions.length) {
     // show result
+    $(".result").show();
     $("#scoreYes").text(scoreYes);
     $("#scoreNo").text(scoreNo);
+    $("#scoreYes").width(scoreYes * 50 + "px");
+    $("#scoreNo").width(scoreNo * 50 + "px");
     $(".button-yes, .button-no, .question").hide();
-  }else {
-  $(".question").text(questions[count]);
+    // how many yes?
+    $(".messageAboutPrivilege").show();
+    
+    if (scoreYes > 6) {
+      $(".messageAboutPrivilege").text(messageAboutPrivilege[0]);
+    } else if (scoreYes < 4) {
+      $(".messageAboutPrivilege").text(messageAboutPrivilege[2]);
+    } else {
+      $(".messageAboutPrivilege").text(messageAboutPrivilege[1]);
+    }
+  } else {
+    $(".question").text(questions[count]);
   }
 });
 
-var questions = ["Are you right handed?",
-                 "Is English your first language?",
-                 "Does one or both of your parents have a college degree?",
-                 "Can you find Band-Aids at mainstream stores designed to blend in with or match your skin tone?",
-                 "Do you rely, or have relied, primarily on public transportation?",
-                 "Have you attended previous schools with people you felt were like yourself?",
-                 "Do you constantly feel unsafe walking alone at night?",
-                 "Does your household employs help as servants, gardeners, etc.?",
-                 "Are you able to move through the world without fear of sexual assault?",
-                 "Have you studied the culture of your ancestors in elementary school?",
-                 "Have you ever been able to play a significant role in a project or activity because of a talent you          gained previously?",  
-                 
-                ];
+var questions = [
+  "Are you right handed?",
+  "Is English your first language?",
+  "Does one or both of your parents have a college degree?",
+  "Can you find Band-Aids at mainstream stores designed to blend in with or match your skin tone?",
+  "Do you rely, or have relied, primarily on public transportation?",
+  "Have you attended previous schools with people you felt were like yourself?",
+  "Do you constantly feel unsafe walking alone at night?",
+  "Does your household employs help as servants, gardeners, etc.?",
+  "Are you able to move through the world without fear of sexual assault?",
+  "Have you studied the culture of your ancestors in elementary school?",
+  "Have you ever been able to play a significant role in a project or activity because of a talent you gained previously?"
+];
+
+var messageAboutPrivilege = ["white p", "don't know", "under P"];
 
 $(".question").text(questions[0]);
